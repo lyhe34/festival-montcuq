36e157ef6e788664862239a1c39dae4fe38f724e
Hal <al_b4n@live.fr>
2023-08-22 16:57:00 +0200

    ble

--- 230821ReactJs/ReactJs4Convertisseur/convertisseur_cels_fahr_kelv/src/App.js
+++ 230821ReactJs/ReactJs4Convertisseur/convertisseur_cels_fahr_kelv/src/App.js
@@ -11,19 +11,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <nav>
+          <ul class="nav">
+
+            <li class="navlist"><a href="SayHello.js" >Say Hello</a></li>
+            <li class="navlist"><a href="Message.js" >Message</a></li>
+            <li class="navlist"><a href="Chrono.js" >Chrono</a></li>
+            <li class="navlist"><a href="Hour.js" >Time</a></li>
+            <li class="navlist"><a href="MonPanier.js" >Mon Panier</a></li>
+
+
+          </ul>
+
+        </nav>
+
+        {/* COMPONENTS */}
+        <Convertisseur />
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+
+
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-
-        {/* COMPONENTS */}
-        <Convertisseur/>
 
 
 
