9647a34b70b7aa9a56aa0123454ac276594afe54
luc <luc.eymard@live.fr>
2023-08-30 15:58:29 +0200

    added hero banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,9 @@ export default function Header()
                 <div className="container-fluid justify-content-around">
                     <a className="navbar-brand text-white" href="#">Festival</a>
                     <ul className="navbar-nav d-flex flex-row">
-                        <li className="nav-item text-white">1</li>
-                        <li className="nav-item text-white">2</li>
-                        <li className="nav-item text-white">3</li>
+                        <li className="nav-item text-white">Accueil</li>
+                        <li className="nav-item text-white">Programme</li>
+                        <li className="nav-item text-white"></li>
                     </ul>
                     <a href='#'><FontAwesomeIcon icon={faUser} className='user-icon'/></a>
                 </div>
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import '../styles/Hero.css';
+
+export default function Hero()
+{
+    return (
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1>Festival</h1>
+                <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+            </div>
+        </div>
+    )
+}
